c7970df63cdf5573f766b497c6697dd98bd073a6
csnascimento <cs_nascimento@yahoo.com.br>
2021-04-14 00:48:46 -0300

    120 - Função para formatar milissegundos em minutos e segundos

--- src/util/Format.js
+++ src/util/Format.js
@@ -8,4 +8,18 @@ class Format{
         return Object.keys(div.firstChild.dataset)[0];
 
     }
+
+    static toTime(duration){
+        
+        let seconds = parseInt((duration/1000)%60);
+        let minutes = parseInt((duration/600000)%60);
+        let hours = parseInt((duration/36000000)%24);
+
+        if (hours < 0){
+            return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        }else{
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+        }
+
+    }
 }
